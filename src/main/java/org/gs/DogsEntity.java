df6e0a6c9670937ff348f93827614260e6b28081
izman.p <p.izman@unifun.com>
2022-04-29 15:52:47 +0300

    removing unnecessary

--- src/main/java/org/gs/DogsEntity.java
+++ src/main/java/org/gs/DogsEntity.java
@@ -1,57 +1,27 @@
 package org.gs;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.hibernate.annotations.LazyToOne;
 
 import javax.persistence.*;
-import javax.transaction.Status;
 import java.util.List;
 
+import static javax.persistence.EnumType.STRING;
 
+enum Color {white, black, brown, red, gold, bicolor, grey, tricolor, merle};
 @Entity
+@Table(name = "dogsentity")
+
 public class DogsEntity extends PanacheEntity {
 
-
-    private String name;
-
-    private String breed;
-
-    private int year;
-
-    private String color;
+    public String name;
+    public int age;
+    @Enumerated(STRING)
+    public Color color;
 
 
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 
 
 }
